226e5a102dd5220f8eb4d49b160cbcbba97bdc76
Oukey <oukey311@gmail.com>
2019-06-09 23:36:31 +0400

    Урок 2. Шаблон + Контекст = html

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def main(request):
-    return render(request, 'mainapp/main.html')
+    return render(request, 'mainapp/main.html', {'username': 'ivan', 'array': [1, 2, 3, 4, 5]})
 
 def products(request):
     return render(request, 'mainapp/products.html')
