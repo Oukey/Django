c0e64e2461ea0d469af759593ae7e960d15799aa
Oukey <oukey311@gmail.com>
2019-06-13 01:11:49 +0400

    Выполнение lesson_3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render
 import json, os
+from .models import Product
 
 JSON_PATH = 'mainapp/json'
 
+
 def loadMenuFromJSON():
-    with open(os.path.join(JSON_PATH, 'menu.json'), 'r', encoding = "utf-8") as infile:
+    with open(os.path.join(JSON_PATH, 'menu.json'), 'r', encoding="utf-8") as infile:
         return json.load(infile)
+
 
 def main(request):
     links_menu = loadMenuFromJSON()
     context = {'links_menu': links_menu, 'username': 'evgenii', 'array': [1, 2, 3, 4, 5]}
     return render(request, 'mainapp/main.html', context)
 
+
 def products(request):
     links_menu = loadMenuFromJSON()
     context = {'links_menu': links_menu}
     return render(request, 'mainapp/products.html', context)
+
 
 def contacts(request):
     links_menu = loadMenuFromJSON()
