120a87bae063baa9aac4b584ed7f91fc2f0ee267
Oukey <oukey311@gmail.com>
2019-06-13 00:24:06 +0400

    Подготовка к lesson_3 (Модели + ORM = данные)

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
+import json, os
 
-# Create your views here.
+JSON_PATH = 'mainapp/json'
+
+def loadMenuFromJSON():
+    with open(os.path.join(JSON_PATH, 'menu.json'), 'r', encoding = "utf-8") as infile:
+        return json.load(infile)
 
 def main(request):
-    return render(request, 'mainapp/main.html', {'username': 'ivan', 'array': [1, 2, 3, 4, 5]})
+    links_menu = loadMenuFromJSON()
+    context = {'links_menu': links_menu, 'username': 'evgenii', 'array': [1, 2, 3, 4, 5]}
+    return render(request, 'mainapp/main.html', context)
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    links_menu = loadMenuFromJSON()
+    context = {'links_menu': links_menu}
+    return render(request, 'mainapp/products.html', context)
 
 def contacts(request):
-    return render(request, 'mainapp/contacts.html')
+    links_menu = loadMenuFromJSON()
+    context = {'links_menu': links_menu}
+    return render(request, 'mainapp/contacts.html', context)
