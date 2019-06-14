75ab66d1707eb0d158727ca596c09d5b287cd01e
Oukey <oukey311@gmail.com>
2019-06-15 01:47:49 +0400

    Обновление lesson_3. Настройка и подключение sqlite3, создание моделей, миграции

--- mainapp/models.py
+++ mainapp/models.py
@@ -6,8 +6,8 @@ class ProductCategory(models.Model):
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
-    name = models.CharField(verbose_name='имя', max_length=64, unique=True)
-    description = models.TextField(verbose_name='описание', blank=True)
+    name = models.CharField(verbose_name='имя категории', max_length=64, unique=True)
+    description = models.TextField(verbose_name='описание категории', blank=True)
 
     def __str__(self):
         return self.name
@@ -18,13 +18,13 @@ class Product(models.Model):
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
 
-    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)  # привязка к категории
     name = models.CharField(verbose_name='имя продукта', max_length=128)
     image = models.ImageField(upload_to='products_images', blank=True)
-    short_desc = models.CharField(verbose_name='кратко', max_length=60, blank=True)
-    description = models.TextField(verbose_name='подробно', blank=True)
+    short_desc = models.CharField(verbose_name='краткое описание', max_length=60, blank=True)
+    description = models.TextField(verbose_name='подробное описание', blank=True)
     price = models.DecimalField(verbose_name='цена', max_digits=8, decimal_places=2, default=0)
-    quantity = models.PositiveIntegerField(verbose_name='склад', default=0)
+    quantity = models.PositiveIntegerField(verbose_name='количество на складе', default=0)
 
     def __str__(self):
         return "{} ({})".format(self.name, self.category.name)
